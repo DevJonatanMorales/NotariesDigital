038efc2f69313af29e9d53655065b32684bae406
Jonatan Morales <moralesorellana170498@gmail.com>
2020-11-10 14:32:11 -0600

    nuevo servicio admin

---
+++ Public/js/admin/nuevoServicio.js
@@ -0,0 +1,92 @@
+/** 
+*
+* Comentario Mostrar
+*
+**/
+const Areas = () => {
+  let datos = {accion: 'getArea'};
+  let layout = '';
+  $.ajax({
+    type: 'POST',
+    url: '../../../Private/Models/AdminModels/nuevoServicio.php',
+    data: {datos},
+    success: function (result) {
+      if (result == 0) {
+        
+      } else {
+        result.forEach((datos) =>{
+          layout += `<option value="${datos.areas_id}">${datos.nom_areas}</option>`;
+        })
+        $("#area").html(layout);
+      }
+    },
+    error: function () {
+      console.log('No se ha podido obtener la inforacion');
+    }
+  })
+}
+
+window.addEventListener('load', Areas);
+
+/** 
+*
+* Comentario Guardar nuevo
+*
+**/
+const form = document.getElementById('formulario');
+const btnGuadarServicio = document.getElementById('btnGuadarServicio');
+let txtServicio = document.getElementById('servicio');
+let cmbAreas = document.getElementById('area');
+
+const valTXT = () => {
+  if (txtServicio.value.length > 3) {
+    btnGuadarServicio.removeAttribute("disabled");
+  } else {
+    btnGuadarServicio.setAttribute("disabled", "");
+  }
+}
+
+txtServicio.addEventListener('keyup', valTXT);
+
+const Nuevo = () => {
+  let datos = {
+    accion: 'nuevo',
+    areas_id: cmbAreas.value,
+    servicio: txtServicio.value
+  }
+  
+  $.ajax({
+    type: 'POST',
+    url: '../../../Private/Models/AdminModels/nuevoServicio.php',
+    data: {datos},
+    success: function (result) {
+      if (result == 1) {
+        Swal.fire({
+          type: 'success',
+          title: 'Éxito',
+          text: 'Servicio creado con exito.',
+          confirmButtonText: 'aceptar',
+          showConfirmButton: true
+        });
+      } else {
+        Swal.fire({
+          type: 'warning',
+          title: 'Advertencia',
+          text: 'Error al crear el servicio.',
+          confirmButtonText: 'aceptar',
+          showConfirmButton: true
+        });
+      }
+    },
+    error: function () {
+      console.log('No se ha podido obtener la informacion');
+    }
+  });
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  Nuevo();
+  form.reset();
+});
+
--- Public/views/AdminView/newServicio.php
+++ Public/views/AdminView/newServicio.php
@@ -10,11 +10,37 @@
 		$link= "servicio";
 		require_once("../Layout/adminMenu.php");
 	?>
-  <div class="container my-4">
-    <h1>nuevo servicio</h1>
-  </div>
+  <main class="container my-4">
+
+    <form class="fondoDos col-sm-12 col-md-5 p-2 mx-auto my-4 text-white rounded" action="#" method="post"
+      id="formulario">
+      <h1 class="text-center border-bottom p-1">Nuevo Servicio</h1>
+
+      <div class="form-group">
+        <label for="servicio">Nuevo servicio</label>
+        <input class="form-control" type="text" id="servicio">
+      </div>
+
+      <div class="form-group">
+        <label for="area">Área</label>
+        <select class="form-control" name="area" id="area">
+
+        </select>
+      </div>
+
+      <div class="form-group">
+        <button class="btn btn-danger" type="submit" id="btnCancelarServicio">
+          Cancelar
+        </button>
+        <button class="btn btn-dark" type="submit" id="btnGuadarServicio" disabled="disabled">
+          Guardar
+        </button>
+      </div>
+    </form>
+  </main>
 
   <script src="../../js/admin/app.js"></script>
+  <script src="../../js/admin/nuevoServicio.js"></script>
 </body>
 
 </html>
