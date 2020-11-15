b57a0cf2f6755441b6e04c250842a4fa9030c3bf
Jonatan Morales <moralesorellana170498@gmail.com>
2020-11-15 17:52:15 -0600

    perfilAbogado

---
+++ Private/Models/AbogadoModels/mostrarPerfil.php
@@ -0,0 +1,53 @@
+<?php 
+require_once("../../Config/config.php");
+require_once("../../Conection/conection.php");
+require_once("../Model/modelFather.php");
+
+class MostrarPerfil extends ModelFather
+{
+  public $resultado;
+  private $datos;
+  private $query = false;
+
+  public function RecibirDatos($datos)
+  {
+    $this->datos = $datos;
+    $this->Accion();
+  }
+
+  private function Accion(){
+    switch ($this->datos['accion']) {
+      case 'mostrar':
+        $this->query = true;
+        $this->Mostrar();
+        break;
+      
+      default:
+        # code...
+        break;
+    }
+  }
+
+  private function Mostrar()
+  {
+    session_start();
+    
+    $sql = "SELECT abogados.nombres, abogados.apellidos, abogados.genero, abogados.fech_naci, abogados.telefono, abogados.direccion, usuarios.foto, usuarios.user, usuarios.email FROM usuarios INNER JOIN abogados ON usuarios.usuario_id=abogados.usuario_id WHERE usuarios.usuario_id = '".$_SESSION['USER_ID']."'";
+
+    $this->PrintJSON($this->Read($sql));
+  }
+
+  private function PrintJSON($stringJson) {
+    header('Content-Type: application/json; charset=utf-8');
+    $this->resultado = json_encode($stringJson);
+  }
+}
+
+$mostrarPerfil = new MostrarPerfil();
+
+if(isset($_POST['datos'])) {
+  $mostrarPerfil->RecibirDatos($_POST['datos']);
+  echo $mostrarPerfil->resultado;    
+}
+
+?>
---
+++ Public/views/AbogadoView/miPerfil.php
@@ -0,0 +1,53 @@
+<?php require_once("./valSesion.php"); ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <?php require_once("../Layout/head.php"); ?>
+</head>
+
+<body>
+  <?php 
+    $link = "perfil";
+    require_once('../Layout/abogadoMenu.php');
+  ?>
+  <div class="container my-4">
+    <div class="card col-ms-12 col-md-8 mx-auto my-4 sombre">
+      <div class="card-header text-center">
+        <h5 class="card-title my-auto">Mi Perfil</h5>
+      </div>
+      <div class="card-body">
+        <img src="../../img/<?php echo $_SESSION['FOTO_USER']; ?>" class="card-img-top border mx-auto d-block"
+          alt="Error al cargar" style="width: 14rem;">
+
+        <div class="row form-group">
+          <p class="card-text col-sm-12 col-md-6" id="txtUser"></p>
+          <p class="card-text col-sm-12 col-md-6" id="txtNombre"></p>
+        </div>
+
+        <div class="row form-group">
+          <p class="card-text col-sm-12 col-md-6" id="txtApellido"></p>
+          <p class="card-text col-sm-12 col-md-6" id="txtGenero"></p>
+        </div>
+
+        <div class="row form-group">
+          <p class="card-text col-sm-12 col-md-6" id="txtEdad"></p>
+          <p class="card-text col-sm-12 col-md-6" id="txtTelefono"></p>
+        </div>
+
+        <div class="row form-group">
+          <p class="card-text col-sm-12 col-md-6" id="txtEmail"></p>
+          <p class="card-text col-sm-12 col-md-6" id="txtDireccion"></p>
+        </div>
+
+        <a href="editarPerfil.php" class="btn btn-dark">Editar perfil</a>
+      </div>
+    </div>
+  </div>
+
+
+  <script src="../../js/abogado/app.js"></script>
+  <script src="../../js/abogado/miPerfil.js"></script>
+</body>
+
+</html>
